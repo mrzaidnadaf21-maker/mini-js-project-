9b51e671e22e0dc3ae119572adc9181145f375f7
mrzaidnadaf21-maker <mrzaidnadaf21@gmail.com>
2025-09-25 02:03:23 +0530

    updated code

--- project1/script.js
+++
@@ -1,7 +0,0 @@
-const container = document.querySelectorAll("span");
-
-container.forEach((dets) => {
-  dets.addEventListener("click", function () {
-    dets.classList.toggle("items");
-  });
-});
